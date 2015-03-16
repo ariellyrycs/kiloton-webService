1f814229ca459775eb89e2738282938573d7bad2
ariellyrycs <ariel_556@hotmail.com>
2015-03-15 23:01:23 -0700

    adding get methods for sprints and users

--- api/routes/users.js
+++ api/routes/users.js
@@ -5,11 +5,15 @@
 /*globals module, require*/
 
 
-var controllers = require('./../controllers/usersController.js');
+var controllers = require('./../controllers/usersController');
 module.exports = function(app) {
     app.get('/users', controllers.findAllUsers);
     app.get('/user/:id', controllers.findById);
     app.post('/user', controllers.addUser);
     app.put('/user/:id', controllers.updateUser);
     app.delete('/user/:id', controllers.deleteUser);
+
+
+    app.get('/user/:userId/sprint/', controllers.findAllSprintsByUser);
+    app.get('/user/:userId/sprint/:sprintId/interaction', controllers.findAllInteractionBySprint);
 };
